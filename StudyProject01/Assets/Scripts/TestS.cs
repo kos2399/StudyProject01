5a3c0e619a00db40792a2b7b6e7443fa63346bdd
FreeStar <freestar@192.168.nate.com>
2016-10-08 14:19:31 +0900

    test

---
+++ StudyProject01/Assets/Scripts/TestS.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestS : MonoBehaviour {
+
+	string Name;
+	string ID;
+	int i;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
